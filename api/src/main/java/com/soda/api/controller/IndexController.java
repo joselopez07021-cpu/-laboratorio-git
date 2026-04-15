49127f3a4a5b4308aa354b93e4738a6581ea207c
Rodrigo <joselopez07021@gmail.com>
2026-04-15 00:28:42 -0600

    Agrega login

--- api/src/main/java/com/soda/api/controller/IndexController.java
+++ api/src/main/java/com/soda/api/controller/IndexController.java
@@ -1,7 +1,10 @@
 package com.soda.api.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IndexController {
@@ -9,5 +12,20 @@ public class IndexController {
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/login")
+    public String login(@RequestParam(required = false) String error, Model model) {
+        model.addAttribute("showError", error != null);
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String processLogin(@RequestParam String username, @RequestParam String password) {
+        if ("admin".equals(username) && "1234".equals(password)) {
+            return "redirect:/";
+        }
+
+        return "redirect:/login?error=true";
     }
 }
